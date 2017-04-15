7209625c5fe6c8873622e65824910fc397007673
wanggen.w <wanggen.w@alibaba-inc.com>
2017-04-15 20:51:17 +0800

    init

--- tmp/BenefitRuleDO.java
+++ tmp/BenefitRuleDO.java
@@ -14,10 +14,5 @@ public class InventorySellerDataDO extends BaseDO {
     private Date     gmtCreate;      // 创建时间
     private Date     gmtModified;    // 修改时间
     private Long     status;         // -1删除，1正常，2停用
-    private String   operator;       // 操作人
-    private String   features;       // 扩展，KV对
-    private Integer  groupId;        // 分组ID
-    private String   description;    // 规则的外化文案
-    private String   name;           // 权益名称
 
 }
